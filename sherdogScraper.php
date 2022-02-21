2f71fec19918385166c5aebf28fe9e683b65d9ee
C4 <56499256+BataBoom@users.noreply.github.com>
2022-02-21 12:46:42 -0600

    Add files via upload
    Update soon to auto pull link, for now you'll manually have to insert sherdog event URL, but it returns only the winners and in json format

---
+++ sherdogScraper.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+use Goutte\Client;
+use Symfony\Component\HttpClient\HttpClient;
+
+$client = new Client();
+$crawler = $client->request('GET', 'https://www.sherdog.com/events/UFC-271-Adesanya-vs-Whittaker-2-90742');
+// Get the latest post in this category and display the titles
+for ($i = 2; $i < 11; ++$i) {
+$crawler->filter('tr:nth-of-type('.$i.') .left span[itemprop="name"]')->each(function ($node) {
+//    $node->text();
+$logz = var_export($node->text());
+$inp = file_get_contents('winners.json');
+$tempArray = json_decode($inp);
+array_push($tempArray, $node->text());
+$jsonData = json_encode($tempArray);
+file_put_contents('winners.json', $jsonData);
+});
+}
